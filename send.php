5cb60bcc752928f23dd328f4a3b3ebc69b344e85
Vikuskin <kiskina.vss@yandex.ru>
2021-03-19 19:06:20 +0300

    create: otherpackages and fixed thankyou

--- send.php
+++ send.php
@@ -1,4 +1,5 @@
 <?php
+if(isset($_POST['name'])) {
 // Файлы phpmailer
 require 'phpmailer/PHPMailer.php';
 require 'phpmailer/SMTP.php';
@@ -8,7 +9,6 @@ require 'phpmailer/Exception.php';
 $name = $_POST['name'];
 $phone = $_POST['phone'];
 $message = $_POST['message'];
-$subscribe = $_POST['subscribe'];
 
 // Формирование самого письма
 $title = "Новое обращение Best Tour Plan";
@@ -17,6 +17,59 @@ $body = "
 <b>Имя:</b> $name<br>
 <b>Почта:</b> $phone<br><br>
 <b>Сообщение:</b><br>$message<br>
+";
+
+// Настройки PHPMailer
+$mail = new PHPMailer\PHPMailer\PHPMailer();
+try {
+    $mail->isSMTP();   
+    $mail->CharSet = "UTF-8";
+    $mail->SMTPAuth   = true;
+    $mail->SMTPDebug = 2;
+    $mail->Debugoutput = function($str, $level) {$GLOBALS['status'][] = $str;};
+
+    // Настройки вашей почты
+    $mail->Host       = 'smtp.gmail.com'; // SMTP сервера вашей почты
+    $mail->Username   = 'besttourplan.kiskina@gmail.com'; // Логин на почте
+    $mail->Password   = 'Bl00dBorn'; // Пароль на почте
+    $mail->SMTPSecure = 'ssl';
+    $mail->Port       = 465;
+    $mail->setFrom('besttourplan.kiskina@gmail.com', 'Виктория Кискина'); // Адрес самой почты и имя отправителя
+
+    // Получатель письма
+    $mail->addAddress('kiskina.vss@yandex.ru');  
+
+// Отправка сообщения
+$mail->isHTML(true);
+$mail->Subject = $title;
+$mail->Body = $body;    
+
+// Проверяем отравленность сообщения
+if ($mail->send()) {$result = "success";} 
+else {$result = "error";}
+
+} catch (Exception $e) {
+    $result = "error";
+    $status = "Сообщение не было отправлено. Причина ошибки: {$mail->ErrorInfo}";
+}
+
+// Отображение результата
+header('Location: thankyou.html');
+}
+
+if(isset($_POST['subscribe'])) {
+// Файлы phpmailer
+require 'phpmailer/PHPMailer.php';
+require 'phpmailer/SMTP.php';
+require 'phpmailer/Exception.php';
+
+// Переменные, которые отправляет пользователь
+$subscribe = $_POST['subscribe'];
+
+// Формирование самого письма
+$title = "Новое обращение Best Tour Plan";
+$body = "
+<h2>Новое обращение</h2>
 <b>Запрос на подписку:</b> $subscribe
 ";
 
@@ -56,3 +109,4 @@ else {$result = "error";}
 
 // Отображение результата
 header('Location: thankyou.html');
+}
