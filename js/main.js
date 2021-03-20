e1af52d741b5d2398f5e72590eaddcfc03bc45a0
Vikuskin <kiskina.vss@yandex.ru>
2021-03-20 15:31:58 +0300

    create: modal

--- js/main.js
+++ js/main.js
@@ -1,3 +1,4 @@
+$(document).ready(function () {
 const hotelSlider = new Swiper('.hotel-slider', {
   // Optional parameters
   loop: true,
@@ -31,10 +32,38 @@ const reviewsSlider = new Swiper('.reviews-slider', {
   
 });
 
-var menuButton = document.querySelector(".menu-button");
-menuButton.addEventListener("click", function () {
-  console.log("Клик");
-  document.querySelector(".navbar-bottom").classList.toggle("navbar-bottom--visible")
+var menuButton = $(".menu-button");
+menuButton.on("click", function () {
+  $(".navbar-bottom").toggleClass("navbar-bottom--visible")
 });
 
 $('.parallax-window').parallax({imageSrc: '../img/newsletter-bg.jpg'});
+
+  var modalButton = $("[data-toggle=modal]");
+  var closeModalButton = $(".modal__close");
+  modalButton.on("click", openModal);
+  closeModalButton.on("click", closeModal);
+
+  function openModal() {
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.addClass('modal__overlay--visible');
+    modalDialog.addClass('modal__dialog--visible');
+  }
+  function closeModal(event) {
+    event.preventDefault();
+    var modalOverlay = $(".modal__overlay");
+    var modalDialog = $(".modal__dialog");
+    modalOverlay.removeClass('modal__overlay--visible');
+    modalDialog.removeClass('modal__dialog--visible');
+  }
+  
+  $(document).on('keydown',function(event) {
+   if (event.keyCode == 27) {
+      var modalOverlay = $(".modal__overlay");
+      var modalDialog = $(".modal__dialog");
+      modalOverlay.removeClass('modal__overlay--visible');
+      modalDialog.removeClass('modal__dialog--visible');
+    }
+});
+});
